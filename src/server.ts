16e99fb60a695e3e961ace61dde96b19e6870a02
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-06-04 23:34:27 -0300

    features:ESLint

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack!' });
-});
+app.get('/', (request, response) => response.json({ message: 'Hello GoStack!' }));
 
 app.listen(3333, () => {
   console.log('Server starded on port 3333 🚀');
