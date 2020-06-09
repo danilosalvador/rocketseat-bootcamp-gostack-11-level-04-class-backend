6c1dd467e8c2d2e5601ea442c07b5c3315d746d9
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-06-08 23:01:44 -0300

    chore:Log Request

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
 
 const app = express();
 
-app.use(routes);
 app.use(express.json());
+
+function logRequest(request: Request, response: Response, next: NextFunction) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next();
+
+  console.timeEnd(logLabel);
+}
+
+app.use(logRequest);
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server starded on port 3333 🚀');
