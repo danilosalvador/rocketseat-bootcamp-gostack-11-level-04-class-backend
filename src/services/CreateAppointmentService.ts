448f9baacc38604037733ea8cd4012c28654a047
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-06-10 22:53:21 -0300

    features:Services & SOLID

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+interface RequestDTO {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentRepository: AppointmentRepository;
+
+  constructor(appointmentRepository: AppointmentRepository) {
+    this.appointmentRepository = appointmentRepository;
+  }
+
+  public execute({ provider, date }: RequestDTO): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('Esse horário já possui agendamento.');
+    }
+
+    const appointment = this.appointmentRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
