b095864fa2c31f9014b2254aebe7b4aba4e3f333
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-06-22 22:08:30 -0300

    features:Repositório do TypeORM

--- src/database/migrations/1592274891282-CreateAppointments.ts
+++ src/database/migrations/1592274891282-CreateAppointments.ts
@@ -13,6 +13,7 @@ export default class CreateAppointments1592274891282
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
