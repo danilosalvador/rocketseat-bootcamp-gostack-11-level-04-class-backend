37571e5be7b1536e62730e6106d5933cf08fa1a6
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-06-23 23:43:22 -0300

    features:Relacionamento nos models

--- src/database/migrations/1592274891282-CreateAppointments.ts
+++ src/database/migrations/1592274891282-CreateAppointments.ts
@@ -10,7 +10,7 @@ export default class CreateAppointments1592274891282
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
@@ -18,12 +18,10 @@ export default class CreateAppointments1592274891282
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
         ],
       }),
